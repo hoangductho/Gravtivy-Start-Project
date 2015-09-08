8791404b5d371eb4e54326d328fe860397370afe
trungnguyen1592 <trung15992@gmail.com>
2015-09-07 17:06:52 -0700

    dang nhap

---
+++ application/modules/login/models/mlogin.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Mlogin extends CI_Model {
+
+	
+
+}
+
+/* End of file mlogin.php */
+/* Location: ./application/modules/login/models/mlogin.php */
